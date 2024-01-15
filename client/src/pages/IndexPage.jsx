b16d97fd382476a7d2b95cd7aa9eff31c404f972
mohdabdulrahman297 <abdulrahmanmohd297@gmail.com>
2024-01-15 13:45:22 +0530

    minor changes

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -25,15 +25,17 @@ export default function IndexPage() {
   return (
     <div className="bg-gray-100 py-8">
       <div className="container mx-auto">
-        <input
-          type="text"
-          placeholder="Search"
-          className="w-full md:w-2/3 p-3 xl:ml-8 mb-8 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 transition duration-300"
-          onChange={(e) => {
-            handleSearch(e.target.value);
-          }}
-        />
-        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 lg:ml-8 gap-8">
+        <div className="w-full md:w-2/3 mx-auto mb-8">
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 transition duration-300"
+            onChange={(e) => {
+              handleSearch(e.target.value);
+            }}
+          />
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8 lg:ml-8">
           {(searchResults.length > 0 ? searchResults : posts).map((post) => (
             <Post key={post._id} {...post} />
           ))}
